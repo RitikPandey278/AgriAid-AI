7b7cc170f79f804d6dff39d6362a3a1da872e7bc
Ritik Pandey <pandeyritik278@gmail.com>
2025-11-29 12:40:32 +0530

    Updated projct with new features

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["https://cerulean-kataifi-b464f5.netlify.app","https://agriaid-ai.onrender.com"], credentials: true }));
 app.use(express.json());
 
 // MongoDB Connection
